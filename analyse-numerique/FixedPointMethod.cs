5c42f5dfd3140250413188bf6e01219d4f5497fe
algorev <algorev@protonmail.com>
2021-03-28 18:13:50 +0200

    Added the fixed point method.

---
+++ analyse-numerique/FixedPointMethod.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace analyse_numerique
+{
+    public class FixedPointMethod : ISolver
+    {
+        private readonly Problem problem;
+
+        public FixedPointMethod(Problem problem)
+        {
+            this.problem = problem;
+        }
+
+        public double IterSolve(int iterations, double start)
+        {
+            double result = start;
+            Console.Write("Starting with: ");
+            Console.Write(start);
+            Console.WriteLine();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                result = Iterate(result);
+                Console.Write(i);
+                Console.Write(". ");
+                Console.WriteLine(result);
+            }
+            Console.Write("Final result: ");
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public double EpsilonSolve(double epsilon, int maxIter, double start)
+        {
+            double currentEstimate = start;
+            double previousEstimate = double.PositiveInfinity;
+            double delta = double.PositiveInfinity; // Make sure delta is greater than epsilon ;)
+
+            Console.Write("Starting with: ");
+            Console.Write(start);
+            Console.WriteLine();
+            Console.Write("Epsilon = ");
+            Console.WriteLine(epsilon);
+
+            for (int i = 0; (delta > epsilon) && (i < maxIter); i++)
+            {
+                currentEstimate = Iterate(currentEstimate);
+
+                Console.Write(i);
+                Console.Write(". ");
+                Console.WriteLine(currentEstimate);
+
+                delta = Math.Abs(previousEstimate - currentEstimate);
+                previousEstimate = currentEstimate;
+
+                Console.Write("Current delta is: ");
+                Console.WriteLine(delta);
+            }
+            Console.Write("Final result: ");
+            Console.WriteLine(currentEstimate);
+            return currentEstimate;
+        }
+
+        private double Iterate(double attempt)
+        {
+            return problem.equation(attempt);
+        }
+    }
+}
--- analyse-numerique/Program.cs
+++ analyse-numerique/Program.cs
@@ -9,10 +9,26 @@ namespace analyse_numerique
             var problem = new Problem(
                 (x) => x*x - 2,
                 (x) => 2*x);
-            var solver = new NewtonsMethod(
+
+            Console.WriteLine("\nTesting out BisectionMethod:");
+            ISolver solver = new BisectionMethod(
+                problem,
+                0,
+                5);
+            solver.EpsilonSolve(0.001, 30, 0);
+            solver.IterSolve(10, 0);
+
+            Console.WriteLine("\nTesting out NewtonsMethod:");
+            solver = new NewtonsMethod(
                 problem);
-            //solver.EpsilonSolve(0.001, 30, 6);
+            solver.EpsilonSolve(0.001, 30, 6);
             solver.IterSolve(10, 6);
+
+            Console.WriteLine("\nTesting out FixedPointMethod:");
+            solver = new FixedPointMethod(
+                problem);
+            solver.EpsilonSolve(0.001, 30, 0.1);
+            solver.IterSolve(10, 0.1);
         }
     }
 }
